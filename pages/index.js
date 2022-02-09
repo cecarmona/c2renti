4dfcfe5a07a29e5f3e7737a7bebf8301597396d1
cecarmona <cecarmona24@gmail.com>
2022-02-09 04:23:17 -0400

    Property Card / Layout

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Flex, Box, Text, Button } from '@chakra-ui/react';
 
+import Property from '../components/Property';
+import { baseUrl, fetchApi } from '../utils/fetchApi';
+
 const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
 	<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
 		<Image src={imageUrl} width={500} height={300} alt="banner"/>
@@ -16,7 +19,7 @@ const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, i
 	</Flex>
 )
 
-export default function Home() {
+export default function Home({ propertiesForSale, propertiesForRent }) {
 	return (
 		<Box>
 			<Banner 
@@ -30,7 +33,7 @@ export default function Home() {
 				imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
 			/>
 			<Flex flexWrap="wrap">
-				{/* Fetch the properties and map over them... */}
+				{propertiesForRent.map((property) => <Property property={property} key={property.id} />)}
 			</Flex>
 			<Banner 
 				purpose="BUY A HOME"
@@ -43,9 +46,20 @@ export default function Home() {
 				imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
 			/>
 			<Flex flexWrap="wrap">
-				{/* Fetch the properties and map over them... */}
+				{propertiesForSale.map((property) => <Property property={property} key={property.id} />)}
 			</Flex>
 		</Box>
 	)
 }
 
+export async function getStaticProps() {
+	const propertyForSale = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`);
+	const propertyForRent = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`);
+
+	return {
+		props: {
+			propertiesForSale: propertyForSale?.hits,
+			propertiesForRent: propertyForRent?.hits
+		}
+	}
+}
